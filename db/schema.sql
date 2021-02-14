edf44ffd05e1e71ed2fb17ec0482509fa52bb4e0
Genis Bardales <genis96@yahoo.com>
2021-02-13 22:44:04 -0500

    mysql DB just need to enter the queries

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,13 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
+CREATE TABLE INSERT INTO cats (name) VALUES ('Molly');
+INSERT INTO cats (name) VALUES ('Charlie');
+INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
+INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
+INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
+INSERT INTO cats (name) VALUES ('Daisy');
+
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
