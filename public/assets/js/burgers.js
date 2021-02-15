9461d233870885eea4f0e24741c2f1fa66d34f75
Genis Bardales <genis96@yahoo.com>
2021-02-15 18:45:11 -0500

    setting up event listeners

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -6,3 +6,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 // updating buttons, linked to the burger-blocl.handlebars
 const changeDevouredBtns = document.querySelectorAll('.change-devoured');
+
+// set event listeners for the create button - CREATE, POST, DELETE
+if(changeDevouredBtns) {
+    changeDevouredBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            // grab id elem (e) - "id"
+            const id = e.target.getAttribute('data-id');
+            const newDevoured = e.target.getAttribute('data-newDevoured');
+            const newDevouredState = {
+                devoured: newDevoured,
+            };
+        })
+    })
+}
+
+
+
