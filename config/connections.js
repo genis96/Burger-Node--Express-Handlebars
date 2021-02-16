63b1c95eb6a6f2e39806cd1e3b8ed2b8d07756c8
Genis Bardales <genis96@yahoo.com>
2021-02-15 20:24:08 -0500

    JAWSDB - created Heroku

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 8000,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'Nightfloat77$',
-  database: 'burgers_db',
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8000,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: 'Nightfloat77$',
+    database: 'burgers_db',
+  });
+};
+
 
 // Make connection.
 connection.connect((err) => {
