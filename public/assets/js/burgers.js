19b66e2e2aae7d9850ad7b6896667486851b98ab
Genis Bardales <genis96@yahoo.com>
2021-02-15 19:18:48 -0500

    burgers.js should be good now

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -4,87 +4,90 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if(event) {
         console.info('DOM LOADED');
     }
-})
 
 // UPDATE/CHANGE button, linked to the burger-blocl.handlebars
-const changeDevouredBtns = document.querySelectorAll('.change-devoured');
+    const changeDevouredBtns = document.querySelectorAll('.change-devoured');
 
-// set event listeners for the create button - UPDATE/CHANGE, CREATE, POST, DELETE
-if(changeDevouredBtns) {
-    changeDevouredBtns.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            // grab id elem (e) - "id"
-            const id = e.target.getAttribute('data-id');
-            const newDevoured = e.target.getAttribute('data-newDevoured');
-            
-            const newDevouredState = {
-                devoured: newDevoured,
+    // set event listeners for the create button - UPDATE/CHANGE, CREATE, POST, DELETE
+    if(changeDevouredBtns) {
+        changeDevouredBtns.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                // grab id elem (e) - "id"
+                const id = e.target.getAttribute('data-id');
+                const newDevoured = e.target.getAttribute('data-newDevoured');
+                
+                const newDevouredState = {
+                    devoured: newDevoured,
+                };
+
+                fetch(`/api/burgers/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    // stringift JSON
+                    body: JSON.stringify(newDevouredState),
+                }).then((response) => {
+                    if(response.ok) {
+                        console.log(`changed devoured to: ${newDevoured}`);
+                        location.reload('/');
+                    } else {
+                        alert('something went wrong!');
+                    }
+                });
+            });
+        });
+    }
+
+// CREATE
+    const createBurgerBtn = document.getElementById('create-form');
+
+    if(createBurgerBtn) {
+        createBurgerBtn.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            // grab textarea value
+            const newBurger = {
+                burger_name: document.getElementById('ca').value.trim(),
+                devoured: document.getElementById('devoured').checked,
             };
-
-            fetch(`/api/burgers/${id}`, {
-                method: 'PUT',
+            //send POST request for new quote
+            fetch('api/burgers', {
+                method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
                 },
-                // stringift JSON
-                body: JSON.stringify(newDevouredState),
-            }).then((response) => {
-                if(response.ok) {
-                    console.log(`changed devoured to: ${newDevoured}`);
-                    location.reload('/');
-                } else {
-                    alert('something went wrong!');
-                }
+                //json stringify 
+                body: JSON.stringify(newBurger),
+            }).then(() => {
+                // empty form
+                document.getElementById('ca').value = '';
+                // reload
+                console.log('Created new burger!');
+                location.reload();
+            });
+        });
+    }
+
+// DELETE
+    const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
+
+    deleteBurgerBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            const id = e.target.getAttribute('data-id');
+
+            // send request
+            fetch(`api/burgers/${id}`, {
+                method: 'DELETE',
+            }).then((res) => {
+                console.log(res);
+                console.log(`Deleted Burger: ${id}`);
+
+                // reload
+                location.reload();
             });
         });
     });
-}
-
-// CREATE
-const createBurgerBtn = document.getElementById('create-form');
-
-if(createBurgerBtn) {
-    createBurgerBtn.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        // grab textarea value
-        const newBurger = {
-            burger_name: document.getElementById('ca').value.trim(),
-            devoured: document.getElementById('devoured').checked,
-        };
-        //send POST request for new quote
-        fetch('api/burgers', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            //json stringify 
-            body: JSON.stringify(newBurger),
-        }).then(() => {
-            // empty form
-            document.getElementById('ca').value = '';
-            // reload
-            console.log('Created new burger!');
-            location.reload();
-        });
-    });
-}
-
-// DELETE
-const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
-
-deleteBurgerBtns.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        const id = e.target.getAttribute('data-id');
-
-        // send request
-        fetch(`api/burgers/${id}`, {
-            method: 'DELETE',
-        }).then((res) => {
-            
-        })
-    })
-})
-
+});
