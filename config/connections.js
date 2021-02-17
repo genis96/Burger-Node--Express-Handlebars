4d8b949d895391b576bbea3aed944ef00a1fb986
Genis Bardales <genis96@yahoo.com>
2021-02-16 23:05:04 -0500

    need help w sql and JAWS on the workbench... also it says internal server error

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
-// let connection;
+let connection;
 
 // working on heroku
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+   connection = mysql.createConnection({
     host: 'localhost',
     port: 8000,
     user: 'root',
