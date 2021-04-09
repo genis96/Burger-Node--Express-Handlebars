37fc559aab923d8b2f56df63851e1bb0d5dc7544
Genis Bardales <genis96@yahoo.com>
2021-04-09 13:04:27 -0400

    updating index handles

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-import express, { static, urlencoded, json } from 'express';
+const express = require('express');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(static('public'));
+app.use(express.static('public'));
 
 // Parse application body as JSON
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set Handlebars.
-import exphbs from 'express-handlebars';
+const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-import routes from './controllers/burgerController.js';
+const routes = require('./controllers/burgerController.js');
 
 app.use(routes);
 
