702972dbca7a18e3c931e90ae31282acaf09ea58
Genis Bardales <genis96@yahoo.com>
2021-04-09 13:51:51 -0400

    trying to fix 500 error

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+import express, { static, urlencoded, json } from 'express';
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
+app.use(static('public'));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 // Set Handlebars.
-const exphbs = require('express-handlebars');
+import exphbs from 'express-handlebars';
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-const routes = require('./controllers/burgerController.js');
+import routes from './controllers/burgerController.js';
 
 app.use(routes);
 
